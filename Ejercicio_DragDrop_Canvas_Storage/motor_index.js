c075791dbfd9d16600ae95c0b06b6e879a9036c8
Alejandrogf <alejandro.garcia.francos@tajamar365.com>
2015-10-05 17:34:33 +0200

    Ejercicio de Drag&Drop, Canvas y Local/Session Storage

---
+++ Ejercicio_DragDrop_Canvas_Storage/motor_index.js
@@ -0,0 +1,20 @@
+﻿document.getElementById("btnAcceder").addEventListener("click", login);
+
+
+
+if (sessionStorage.getItem("nombre")) {
+    location.replace("registroClase.html");
+}
+
+
+function login() {
+    if (document.getElementById("txtNombre").value == "") {
+        alert("Nombre vacio");
+        $("#txtNombre").val("");
+        return;
+    }
+    sessionStorage.setItem("nombreAcceso", document.getElementById("txtNombre").value);
+    location.replace("registroClase.html");
+
+}
+
---
+++ Ejercicio_DragDrop_Canvas_Storage/motor_registroClase.js
@@ -0,0 +1,23 @@
+﻿//if (!sessionStorage.getItem("nombre")) {
+//    location.replace("index.html");
+//}
+
+
+function dibujarMesa() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    //Color
+    var e = document.getElementById("color_id");
+    ctx.fillStyle = e.options[e.selectedIndex].value;
+    //Dibujar rectangulo, con posX, posY, alto, ancho
+
+    var y = $("#txtPosY").val();
+
+    ctx.fillRect(document.getElementById("#txtPosX"), y, $("#txtAlto").val(), $("#txtAncho").val());
+};
+
+$(document).ready(function () {
+
+    $("#btnRegistrar").click(dibujarMesa);
+
+});
